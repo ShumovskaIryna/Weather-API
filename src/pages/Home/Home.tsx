0bf6fb9459f5736051647dbf9476bed5947451af
Iryna Shumovska <shumovska_ia@groupbwt.com>
2022-12-29 16:03:55 +0200

    change  Home Page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,30 +1,15 @@
 import React from 'react'
-// import { useCustomDispatch, useCustomSelector } from '../../hooks/store';
-// import { selectCurrentWeatherData } from '../../store/selectors';
-// import { fetchCurrentWeather } from '../../store/thunks/fetchCurrentWeather';
-// import { Days } from './components/Days/Days';
-// import { ThisDay } from './components/ThisDay/ThisDay';
-// import { ThisDayInfo } from './components/ThisDayInfo/ThisDayInfo';
 
 import s from './Home.module.scss'
 
 interface IAppProps {}
 
 export const Home: React.FC<IAppProps> = (props) => {
-  //   const dispatch = useCustomDispatch();
-  //   const { weather } = useCustomSelector(selectCurrentWeatherData);
-
-  //   useEffect(() => {
-  //     dispatch(fetchCurrentWeather('paris'));
-  //   }, []);
   return (
     <div className={s.home}>
       <div className={s.wrapper}>
-        {/* <ThisDay weather={weather} /> */}
-        {/* <ThisDayInfo /> */}
         Home
       </div>
-      {/* <Days /> */}
     </div>
   )
 }
