bee7d53b3ea3ba639a198911d61f1b2424aab8bd
Iryna Shumovska <shumovska_ia@groupbwt.com>
2022-12-29 16:05:20 +0200

    add Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-function App() {
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { Details } from './pages/Details/Details'
+import { Home } from './pages/Home/Home'
+
+function App (): JSX.Element {
   return (
     <div className="App">
-      <header className="App-header">
-          Hello
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/details" element={<Details />} />
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
