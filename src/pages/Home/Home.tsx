69e531e44fe41c77788a75db634ced845cdaa0bd
Iryna Shumovska <shumovska_ia@groupbwt.com>
2022-12-31 01:14:11 +0200

    add Home Page style

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
+import Cards from '../../components/Cards/Cards'
+import Header from '../../components/Header/Header'
 
-import s from './Home.module.scss'
+import './Home.css'
 
 interface IAppProps {}
 
 export const Home: React.FC<IAppProps> = (props) => {
   return (
-    <div className={s.home}>
-      <div className={s.wrapper}>
-        Home
+    <div className="homeContainer">
+      <div className="wrapper">
+        <Header/>
+      </div>
+      <div className="wrapper">
+        <Cards/>
       </div>
     </div>
   )
