46c44d10da7855339c7dd6f65efaac1b2832060c
Iryna Shumovska <shumovska_ia@groupbwt.com>
2023-01-01 18:30:55 +0200

    Create list with conditions

---
+++ src/components/DetailCards/ThisDayItem.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Item } from './DetailCardCondition'
+import Typography from '@mui/material/Typography'
+import IndicatorSvgSelector from '../../assets/icons/indicators/IndicatorSvgSelector'
+import Grid from '@mui/material/Grid'
+
+interface Props {
+  item: Item
+}
+
+function ThisDayItem ({ item }: Props): JSX.Element {
+  const { iconId, name, value } = item
+  return (
+        <div className="conditions">
+            <Grid container spacing={1}>
+                <Grid item xs={1}>
+                    <Typography sx={{ mb: 2, fontSize: 18 }} color="text.secondary">
+                        <IndicatorSvgSelector id={iconId} />
+                    </Typography>
+                </Grid>
+                <Grid item xs={9}>
+                    <Typography sx={{ mb: 2, fontSize: 16 }} color="text.secondary">
+                        {name} : {value}
+                    </Typography>
+                </Grid>
+            </Grid>
+        </div>
+  )
+}
+
+export default ThisDayItem
