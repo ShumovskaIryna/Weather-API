1fe246e344890ee42d78c5aefdf49d88744162df
Iryna Shumovska <shumovska_ia@groupbwt.com>
2023-01-05 16:20:33 +0200

    change some  svg selectors

--- src/assets/icons/global/GlobalSvgSelector.tsx
+++ src/assets/icons/global/GlobalSvgSelector.tsx
@@ -9,8 +9,8 @@ export default function GlobalSvgSelector ({ id }: Props): JSX.Element {
     case '01n':
       return (
         <svg
-          width="90"
-          height="90"
+          width="120"
+          height="120"
           viewBox="0 0 48 48"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
@@ -34,11 +34,86 @@ export default function GlobalSvgSelector ({ id }: Props): JSX.Element {
           </defs>
         </svg>
       )
+
+    case '02n':
+      return (
+        <svg
+          width="120"
+          height="120"
+          viewBox="0 0 48 48"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M32 24C36.4183 24 40 20.4183 40 16C40 11.5817 36.4183 8 32 8C27.5817 8 24 11.5817 24 16C24 20.4183 27.5817 24 32 24Z"
+            fill="url(#paint5_linear)"
+          />
+          <path
+            d="M30.96 1.45999L31.48 4.41399C31.828 6.38399 28.872 6.90399 28.526 4.93399L28.006 1.97999C27.658 0.00998566 30.614 -0.510014 30.96 1.45999ZM41.546 4.98199L39.826 7.43999C38.678 9.07799 36.222 7.35799 37.368 5.71999L39.088 3.26199C40.236 1.62399 42.692 3.34399 41.546 4.98199ZM20.984 6.45399L23.442 8.17399C25.08 9.32199 23.36 11.778 21.722 10.632L19.264 8.91199C17.626 7.76399 19.346 5.30799 20.984 6.45399ZM42.282 21.366L44.74 23.086C46.378 24.234 44.658 26.69 43.02 25.544L40.562 23.824C38.924 22.676 40.644 20.22 42.282 21.366ZM46.542 14.958L43.588 15.478C41.618 15.826 41.098 12.87 43.068 12.524L46.022 12.004C47.992 11.656 48.512 14.612 46.542 14.958Z"
+            fill="#FFB300"
+          />
+          <path
+            d="M12 34C7.582 34 4 30.418 4 26C4 21.582 7.582 18 12 18C12.834 18 13.636 18.128 14.392 18.364C15.518 14.68 18.946 12 23 12C27.97 12 32 16.03 32 21C32 21.47 31.964 21.93 31.894 22.38C32.55 22.134 33.258 22 34 22C37.314 22 40 24.686 40 28C40 31.314 37.314 34 34 34H12Z"
+            fill="url(#paint1_linear)"
+          />
+          <path
+            d="M14 21C14 25.97 18.03 30 23 30C27.502 30 31.23 26.696 31.894 22.38C32.55 22.134 33.258 22 34 22C37.314 22 40 24.686 40 28C40 31.314 37.314 34 34 34H12C7.582 34 4 30.418 4 26C4 21.582 7.582 18 12 18C12.834 18 13.636 18.128 14.392 18.364C14.136 19.198 14 20.082 14 21Z"
+            fill="url(#paint2_radial)"
+          />
+          <defs>
+          <linearGradient
+              id="paint5_linear"
+              x1="24.0092"
+              y1="12.5099"
+              x2="24.0092"
+              y2="35.1951"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stopColor="#FFC227" />
+              <stop offset="1" stopColor="#FFB300" />
+            </linearGradient>
+            <linearGradient
+              id="paint0_linear"
+              x1="40"
+              y1="8"
+              x2="28.7832"
+              y2="19.0723"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stopColor="#FFB301" />
+              <stop offset="1" stopColor="#FFC533" />
+            </linearGradient>
+            <linearGradient
+              id="paint1_linear"
+              x1="22"
+              y1="12"
+              x2="22"
+              y2="34"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stopColor="#9FC7FF" />
+              <stop offset="1" stopColor="#9BC1F5" />
+            </linearGradient>
+            <radialGradient
+              id="paint2_radial"
+              cx="0"
+              cy="0"
+              r="1"
+              gradientUnits="userSpaceOnUse"
+              gradientTransform="translate(15.0679 -22.6966) rotate(-90) scale(20.9574 6.69705)"
+            >
+              <stop stopColor="#486DA8" stopOpacity="0.4" />
+              <stop offset="1" stopColor="#486DA8" stopOpacity="0" />
+            </radialGradient>
+          </defs>
+        </svg>
+      )
+
     case '03n':
       return (
         <svg
-          width="90"
-          height="90"
+          width="120"
+          height="120"
           viewBox="0 0 48 48"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
@@ -80,8 +155,8 @@ export default function GlobalSvgSelector ({ id }: Props): JSX.Element {
     case '04n':
       return (
         <svg
-          width="90"
-          height="90"
+          width="120"
+          height="120"
           viewBox="0 0 48 48"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
@@ -128,8 +203,8 @@ export default function GlobalSvgSelector ({ id }: Props): JSX.Element {
     case '09n':
       return (
         <svg
-          width="90"
-          height="90"
+          width="120"
+          height="120"
           viewBox="0 0 48 48"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
@@ -175,8 +250,8 @@ export default function GlobalSvgSelector ({ id }: Props): JSX.Element {
     case '10n':
       return (
         <svg
-          width="90"
-          height="90"
+          width="120"
+          height="120"
           viewBox="0 0 48 48"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
@@ -222,8 +297,8 @@ export default function GlobalSvgSelector ({ id }: Props): JSX.Element {
     case '01d':
       return (
           <svg
-            width="90"
-            height="90"
+            width="120"
+            height="120"
             viewBox="0 0 48 48"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
@@ -254,8 +329,8 @@ export default function GlobalSvgSelector ({ id }: Props): JSX.Element {
     case '02d':
       return (
           <svg
-            width="90"
-            height="90"
+            width="120"
+            height="120"
             viewBox="0 0 48 48"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
@@ -328,8 +403,8 @@ export default function GlobalSvgSelector ({ id }: Props): JSX.Element {
     case '03d':
       return (
           <svg
-            width="90"
-            height="90"
+            width="120"
+            height="120"
             viewBox="0 0 48 48"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
@@ -371,8 +446,8 @@ export default function GlobalSvgSelector ({ id }: Props): JSX.Element {
     case '04d':
       return (
           <svg
-            width="90"
-            height="90"
+            width="120"
+            height="120"
             viewBox="0 0 48 48"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
@@ -419,8 +494,8 @@ export default function GlobalSvgSelector ({ id }: Props): JSX.Element {
     case '09d':
       return (
           <svg
-            width="90"
-            height="90"
+            width="120"
+            height="120"
             viewBox="0 0 48 48"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
@@ -466,8 +541,8 @@ export default function GlobalSvgSelector ({ id }: Props): JSX.Element {
     case '10d':
       return (
           <svg
-            width="90"
-            height="90"
+            width="120"
+            height="120"
             viewBox="0 0 48 48"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
@@ -543,8 +618,8 @@ export default function GlobalSvgSelector ({ id }: Props): JSX.Element {
     default:
       return (
         <svg
-          width="90"
-          height="90"
+          width="120"
+          height="120"
           viewBox="0 0 48 48"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
